0d4ed9ff3035ef36016305060dffe06f7b06d8db
mous90245-hue <mous90245@gmail.com>
2026-05-12 23:01:53 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, "..");
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [...tanstackStart(), react(), cloudflare()],
+  resolve: {
+    alias: [
+      {
+        find: /^@\/(.*)/,
+        replacement: resolve(__dirname, "src/$1"),
+      },
+    ],
+  },
 });
